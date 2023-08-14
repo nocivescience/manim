d86b88b7a55131fc65e2ef71ee7d2292bc29dc77
ricardo <nocive.science@gmail.com>
2023-08-14 02:09:15 -0400

    primer commit

--- ambar.py
+++ ambar.py
@@ -6,7 +6,7 @@ class Ambar(Scene):
         'colors':[YELLOW,BLUE,TEAL,GREEN,RED]
     }
     def construct(self):
-        square=Square(side_length=4)
+        square=Square(side_length=2)
         self.play(Create(square))
         dots=self.get_dots(square)
         self.get_anim(dots)
@@ -30,14 +30,26 @@ class Ambar(Scene):
         return dots
     def get_anim(self,dots):
         time=0
-        while time<10:
+        while time<100:
+            positions=[]
+            for particle in dots:
+                posicion=rotate_vector(0.08*RIGHT,2*np.pi*np.random.random())+particle.get_center()+0.01*RIGHT
+                if posicion[0]>1:
+                    posicion[0]=-1
+                if posicion[0]<-1:
+                    posicion[0]=1
+                if posicion[1]>1:
+                    posicion[1]=-1
+                if posicion[1]<-1:
+                    posicion[1]=1
+                positions.append(posicion)
             myTime=.5
             time+=myTime
             self.play(*it.chain(*[
-                    (particle.animate.rotate(TAU*np.random.random())
-                    .move_to(rotate_vector(0.03*RIGHT,2*np.pi*np.random.random())+particle.get_center()+0.01*RIGHT)
+                    (particle.animate.rotate(TAU*.2*np.random.random())
+                    .move_to(pos)
                     ,)
-                    for particle in dots
+                    for particle, pos in zip(dots, positions)
                 ]), run_time=myTime
             )
         self.wait()
