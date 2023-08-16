b2250c6d66081c95a8a5dada0a9c21c5a4043b8a
ricardo <nocive.science@gmail.com>
2023-08-16 00:34:03 -0400

    terminado la carga e

--- conteoBolaAmarilla.py
+++ conteoBolaAmarilla.py
@@ -4,26 +4,40 @@ class MyPRoject(Scene):
     conf={
         'n_balls':2,
         'color_a':YELLOW,
-        'color_b':RED
+        'color_b':RED,
+        'radius_balls': .4,
+        'radius_ball': .2,
     }
     def construct(self):
-        balls=self.get_balls()
-        dot=self.get_ball(balls)
-        self.add(dot)
-        self.play(Create(balls))
-        self.wait(4)
+        balls=self.balls=self.get_balls()
+        ball=self.ball_main().set_z_index(2)
+        self.get_ball(ball)
+        self.add(ball,balls)
+        self.wait(6)
+    def ball_main(self):
+        radius=self.conf['radius_ball']
+        ball=Dot(radius=radius,color=self.conf['color_b'])
+        ball.center=ball.get_center()
+        ball.radius=radius
+        return ball
     def get_balls(self):
         balls=VGroup()
-        for _ in range(self.conf['n_balls']):
-            ball=Dot(radius=0.4)
+        positions=np.array([[-4,0,0],[4,0,0]])
+        for _,pos in zip(range(self.conf['n_balls']),positions):
+            radius=self.conf['radius_balls']
+            ball=Dot(radius=radius)
+            ball.radius=radius
             ball.set_color(self.conf['color_a'])
+            ball.position=pos
+            ball.move_to(pos)
             balls.add(ball)
-        balls.arrange(RIGHT,buff=4)
         return balls
-    def get_ball(self,balls):
-        dot=Dot().move_to(ORIGIN)
+    def get_ball(self,ball):
         def dot_update(mob,dt):
-            mob.get_center()[0]+=dt*4
-            mob.move_to(mob.get_center())
-        dot.add_updater(dot_update)
-        return dot
+            if mob.center[0]-mob.radius<self.balls[0].position[0]+self.balls[0].radius:
+                mob.center[0]+=dt*4
+            if mob.center[0]+mob.radius>self.balls[1].position[0]-self.balls[0].radius:
+                mob.center[0]-=dt*4
+            mob.move_to(mob.center)
+        ball.add_updater(dot_update)
+        return ball
--- deduciendoLaCargaE.py
+++ deduciendoLaCargaE.py
@@ -7,7 +7,7 @@ class MyNumberRect(Scene):
             include_tip=True,
             include_numbers=True,
         )
-        texto1=MathTex('\\cdot 10^{+03}')
+        texto1=MathTex('\\cdot 10^{+03}', font_size=30)
         texto1.next_to(lin1,RIGHT,buff=0.2)
         lin1.add(texto1)
         lin2=NumberLine(
@@ -16,32 +16,70 @@ class MyNumberRect(Scene):
             include_tip=True,
             include_numbers=True,
         )
-        texto2=MathTex('\\cdot 10^{+23}')
+        lin3=NumberLine(
+            x_range=[0,100,10],
+            length=config['frame_width']-1,
+            include_tip=True,
+            include_numbers=True,
+        )
+        lin4=NumberLine(
+            x_range=[0,160,16],
+            length=config['frame_width']-1,
+            include_tip=True,
+            include_numbers=True,
+        )
+        texto2=MathTex('\\cdot 10^{+23}', font_size=30)
         texto2.next_to(lin2,RIGHT,buff=0.2)
+        texto3= MathTex('\\cdot 10^{+02}', font_size=30)
+        texto3.next_to(lin3,RIGHT,buff=0.2)
+        texto4= MathTex('\\cdot 10^{+06}', font_size=30)
+        texto4.next_to(lin4,RIGHT,buff=0.2)
         lin2.add(texto2)
-        self.play(Create(VGroup(lin1,lin2).arrange(DOWN,buff=3).scale(0.6)))
+        lin3.add(texto3)
+        lin4.add(texto4)
+        VGroup(lin1,lin2,lin3,lin4).set_color(RED).scale(.4)
+        self.play(Create(VGroup(lin1,lin2,lin3,lin4).arrange(DOWN,buff=3).scale(0.6)))
         self.wait()
         x_value=ValueTracker(6)
         fx=lambda x:x.get_value()*1.6
+        gx=lambda x:x.get_value()*1.2
         fx_value=ValueTracker(fx(x_value))
-        decimal1=DecimalNumber(x_value.get_value()).add_updater(
+        gx_value=ValueTracker(gx(x_value))
+        hx_value=ValueTracker(fx(x_value)+gx(x_value))
+        decimal1=DecimalNumber(x_value.get_value(), font_size=30).add_updater(
             lambda t: t.set_value(x_value.get_value())
         )
-        decimal2=DecimalNumber(fx_value.get_value()).add_updater(
+        decimal2=DecimalNumber(fx_value.get_value(), font_size=30).add_updater(
             lambda t: t.set_value(fx_value.get_value())
+        )
+        decimal3=DecimalNumber(gx_value.get_value(), font_size=30).add_updater(
+            lambda t: t.set_value(gx_value.get_value())
+        )
+        decimal4=DecimalNumber(hx_value.get_value(), font_size=30).add_updater(
+            lambda t: t.set_value(hx_value.get_value())
         )
         decimal22=decimal2.copy()
         decimal11=decimal1.copy()
-        my_tip=Arrow().rotate(3*PI/2)
+        decimal33=decimal3.copy()
+        decimal44=decimal4.copy()
+        my_tip=Arrow().rotate(3*PI/2).scale(0.6)
         my_tip.set_width(0.08)
         my_tip.set_height(0.8)
         my_tip2=my_tip.copy()
+        my_tip3=my_tip.copy()
+        my_tip4=my_tip.copy()
         my_tip.add_updater(lambda m: m.next_to(lin1.n2p(x_value.get_value()),UP,buff=0))
         my_tip2.add_updater(lambda m: m.next_to(lin2.n2p(fx_value.get_value()),UP,buff=0))
+        my_tip3.add_updater(lambda m: m.next_to(lin3.n2p(gx_value.get_value()),UP,buff=0))
+        my_tip4.add_updater(lambda m: m.next_to(lin4.n2p(hx_value.get_value()),UP,buff=0))
         decimal1.add_updater(lambda t: t.next_to(my_tip,UP,buff=0.2))
         decimal2.add_updater(lambda t: t.next_to(my_tip2,UP,buff=0.2))
+        decimal3.add_updater(lambda t: t.next_to(my_tip3,UP,buff=0.2))
+        decimal4.add_updater(lambda t: t.next_to(my_tip4,UP,buff=0.2)) 
         self.play(Create(my_tip),Create(decimal1))
         self.play(Create(my_tip2),Create(decimal2))
+        self.play(Create(my_tip3),Create(decimal3))
+        self.play(Create(my_tip4),Create(decimal4))
         self.wait()
         time=0
         FARADAY=VGroup(
@@ -52,21 +90,39 @@ class MyNumberRect(Scene):
             Text('N° de electrones:'),
             decimal22
         ).arrange(RIGHT,buff=0.1)
-        myConstants=VGroup(FARADAY,AVOGRADO).arrange(RIGHT,buff=0.4).to_edge(DOWN,buff=0.14)
+        FUENTES=VGroup(
+            Text('Fuentes:'),
+            decimal33
+        ).arrange(RIGHT,buff=0.1)
+        AANI=VGroup(
+            Text('Años:'),
+            decimal44
+        ).arrange(RIGHT,buff=0.1)
+        myConstants=VGroup(FARADAY,AVOGRADO,FUENTES,AANI).arrange(RIGHT,buff=0.4).to_edge(DOWN,buff=0.14)
         FARADAY[0].scale(0.6)
         AVOGRADO[0].scale(0.6)
+        FUENTES[0].scale(0.6)
+        AANI[0].scale(0.6)
+        myConstants.set_width(config['frame_width'])
         self.play(Write(myConstants))
-        while time<20:
+        while time<10:
             my_time=.5
             time+=my_time
             random1=np.random.uniform(-1,1)
             random2=np.random.uniform(-1,1)
+            random3=np.random.uniform(-1,1)
+            random4=np.random.uniform(-1,1)
             self.play(
                 x_value.animate.set_value(6+random1),
                 decimal1.animate.set_value(6+random1),
                 fx_value.animate.set_value(1.6+random2),
                 decimal2.animate.set_value(1.6+random2),
                 decimal11.animate.set_value(random1),
-                decimal22.animate.set_value(random2)
-            ,run_time=my_time,rate_functio=smooth)
+                gx_value.animate.set_value(1.2+random2),
+                decimal22.animate.set_value(random2),
+                decimal33.animate.set_value(random3),
+                hx_value.animate.set_value(1.6+1.2+random2+random3),
+                decimal4.animate.set_value(1.6+1.2+random2+random3),
+                decimal44.animate.set_value(random2+random3),
+            run_time=my_time,rate_functio=smooth)
         self.wait()
